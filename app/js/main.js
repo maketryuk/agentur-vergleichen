6138c62dc456294bc0cef6388f0c3b4ed39b5038
maketryuk <nevertry.design@gmail.com>
2021-09-22 13:17:37 +0300

    Add validate on third steps

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,48 @@
-"use strict";$((function(){$(".btn-next").click((function(){$(this).parent().parent().addClass("hidden"),$(this).parent().parent().removeClass("show"),$(this).parent().parent().next(".container").addClass("show")})),$(".btn-prev").click((function(){$(this).parent().parent().addClass("hidden"),$(this).parent().parent().removeClass("show"),$(this).parent().parent().prev(".container").addClass("show")})),$(".scroll-to").click((function(t){t.preventDefault(),$("html, body").animate({scrollTop:$(".steps").offset().top},800)}))})),$((function(){$(".burger").click((function(){$(".burger").toggleClass("burger--active"),$("body").toggleClass("lock")}))}));
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+
+$(function () {
+  $('.btn-next').click(function () {
+    $(this).parent().parent().addClass('hidden');
+    $(this).parent().parent().removeClass('show');
+    $(this).parent().parent().next('.container').addClass('show');
+  });
+  $('.btn-prev').click(function () {
+    $(this).parent().parent().addClass('hidden');
+    $(this).parent().parent().removeClass('show');
+    $(this).parent().parent().prev('.container').addClass('show');
+  });
+  $(".scroll-to").click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $(".steps").offset().top
+    }, 800);
+  });
+  $('.btn-next-validate').click(function () {
+    if ($('.input-validate').length && $('.input-validate').val().length) {
+      $('.input-validate').removeClass('not-validate');
+      $(this).parent().parent().addClass('hidden');
+      $(this).parent().parent().removeClass('show');
+      $(this).parent().parent().next('.container').addClass('show');
+    } else {
+      $('.input-validate').addClass('not-validate');
+      $(this).parent().parent().removeClass('hidden');
+      $(this).parent().parent().addClass('show');
+      $(this).parent().parent().next('.container').removeClass('show');
+    }
+  });
+});
+"use strict";
+
+$(function () {
+  $('.burger').click(function () {
+    $('.burger').toggleClass('burger--active');
+    $('body').toggleClass('lock');
+  });
+});
+//# sourceMappingURL=main.js.map
--- src/blocks/modules/steps/steps.js
+++ src/blocks/modules/steps/steps.js
@@ -17,4 +17,18 @@ $(function () {
         scrollTop: $(".steps").offset().top
     }, 800);
   });
+
+  $('.btn-next-validate').click(function() {
+    if($('.input-validate').length && $('.input-validate').val().length) {
+      $('.input-validate').removeClass('not-validate')
+      $(this).parent().parent().addClass('hidden');
+      $(this).parent().parent().removeClass('show');
+      $(this).parent().parent().next('.container').addClass('show');
+    } else {
+      $('.input-validate').addClass('not-validate')
+      $(this).parent().parent().removeClass('hidden');
+      $(this).parent().parent().addClass('show');
+      $(this).parent().parent().next('.container').removeClass('show');
+    }
+  });
 })
