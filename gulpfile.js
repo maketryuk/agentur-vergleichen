4bdcf48fd3fb8d3dcd4c6f0ded308a61e678a257
maketryuk <nevertry.design@gmail.com>
2021-09-22 11:51:55 +0300

    Version 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -47,9 +47,6 @@ const stylesBuild = () => {
     .pipe(sass({
       outputStyle: 'expanded'
     }).on('error', notify.onError()))
-    .pipe(rename({
-      suffix: '.min'
-    }))
     .pipe(autoprefixer({
       cascade: false,
     }))
